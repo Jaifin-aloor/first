7146eba7571002e50edd467eee7e90c9329435e1
Jaifin-aloor <aloorjaifin@gmail.com>
2026-01-28 20:53:29 +0530

    added router to express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
-console.log("hello world! - my first typescript app")
+import http from "http";
+import express from 'express';
+import type {Express, Request, Response} from 'express';
+import cors from 'cors';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world from Express");
+});
+
+// ROUTES
+
+const router = express.Router();
+
+router.get("/tasks", (req: Request, res: Response) => {
+    res.status(200).json([]);
+})
+router.get("/tasks/:id", (req: Request, res: Response) => {
+    res.status(200).json([]);
+})
+router.post("/tasks", (req: Request, res: Response) => {
+    res.status(200).json([]);
+})
+router.put("/tasks/:id", (req: Request, res: Response) => {
+    res.status(200).json([]);
+})
+router.delete("/tasks/:id", (req: Request, res: Response) => {
+    res.status(200).json([]);
+})
+
+
+// adding router to express app
+app.use(router);
+
+http.createServer(app).listen(3000, () => {
+    console.log("server is running on port 3000");
+});
